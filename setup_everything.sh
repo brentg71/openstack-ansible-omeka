0d30854e946e88cc15e1f11d2fae779636d4ef72
brentg71 <brent.gawryluik@ubc.ca>
2017-07-13 15:08:27 -0700

    fixed 'random state' error

--- setup_everything.sh
+++ setup_everything.sh
@@ -3,9 +3,15 @@
 # Script variables
 PASSWD_GEN_CMD="openssl rand -base64 16"
 
+touch .rnd
+PWD=`pwd`
+export RANDFILE="${PWD}/.rnd"
+
 # Generate random passwords for MySQL accounts
 MYSQL_ROOT_PASSWORD=`${PASSWD_GEN_CMD}`
 MYSQL_OMEKA_PASSWORD=`${PASSWD_GEN_CMD}`
+
+unset RANDFILE
 
 # Install requisite Ansible Galaxy roles
 ansible-galaxy install -r requirements.yml
